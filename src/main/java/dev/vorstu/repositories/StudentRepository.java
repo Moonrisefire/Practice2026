a31e983bcb46bfb3be44c693ac46d7b6d15c8f45
Moonrisefire <vitalijsibrin@gmail.com>
2026-07-15 16:56:28 +0400

    Delete src/main directory

--- src/main/java/dev/vorstu/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.vorstu.repositories;
-
-import dev.vorstu.dto.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-    Iterable<Student> findByGroup(String group);
-}
